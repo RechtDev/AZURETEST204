d4f16e1782070f0551021aa286da356b30ad3fd0
RechtDev <rechtproductions@gmail.com>
2022-09-16 02:14:17 -0400

    INIT

---
+++ AzureTest204SQL/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureTest204SQL
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char Gender { get; set; }
+    }
+}
---
+++ AzureTest204SQL/Services/EmployeeService.cs
@@ -0,0 +1,11 @@
+﻿namespace AzureTest204SQL.Services
+{
+    public class EmployeeService
+    {
+        private static string _connString = "";
+        private static string db_user = "recht-admin";
+        private static string db_password = "DpF@3373791@11";
+        private static string db_database = "";
+
+    }
+}
